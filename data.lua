5e624dffe04280932174b53c899db18f75fbd55d
jingleheimer-schmidt <46385945+jingleheimer-schmidt@users.noreply.github.com>
2023-11-01 22:31:05 -0700

    Create data.lua

---
+++ data.lua
@@ -0,0 +1,34 @@
+
+local little_spider_arguments = {
+    scale = 1/3,
+    leg_scale = 1/2,
+    name = "little-spidertron",
+    leg_thickness = 2,
+    leg_movement_speed = 3,
+}
+create_spidertron(little_spider_arguments)
+local little_spider_entity = data.raw["spider-vehicle"]["little-spidertron"]
+little_spider_entity.guns = nil
+little_spider_entity.inventory_size = 0
+little_spider_entity.trash_inventory_size = 0
+little_spider_entity.equipment_grid = nil
+little_spider_entity.allow_passengers = false
+little_spider_entity.is_military_target = false
+little_spider_entity.collision_box = {{-0.005, -0.005}, {0.005, 0.005}}
+
+local little_spider_recipe = table.deepcopy(data.raw["recipe"]["spidertron"])
+little_spider_recipe.name = "little-spidertron"
+little_spider_recipe.ingredients = {
+    {"electronic-circuit", 4},
+    {"iron-plate", 12},
+    {"inserter", 8},
+    {"raw-fish", 1},
+}
+little_spider_recipe.result = "little-spidertron"
+little_spider_recipe.enabled = true
+data:extend{little_spider_recipe}
+
+local little_spider_item = table.deepcopy(data.raw["item-with-entity-data"]["spidertron"])
+little_spider_item.name = "little-spidertron"
+little_spider_item.place_result = "little-spidertron"
+data:extend{little_spider_item}
