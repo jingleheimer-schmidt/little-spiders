3098b09aaa7f0733de2f4727be2d2a01c1322a37
jingleheimer-schmidt <46385945+jingleheimer-schmidt@users.noreply.github.com>
2023-11-05 00:19:19 -0700

    Create colors.lua

---
+++ util/colors.lua
@@ -0,0 +1,24 @@
+
+local red = { r = 1, g = 0, b = 0 }
+local orange = { r = 1, g = 0.5, b = 0 }
+local yellow = { r = 1, g = 1, b = 0 }
+local green = { r = 0, g = 1, b = 0 }
+local blue = { r = 0, g = 0, b = 1 }
+local purple = { r = 0.5, g = 0, b = 1 }
+local pink = { r = 1, g = 0, b = 1 }
+local white = { r = 1, g = 1, b = 1 }
+local black = { r = 0, g = 0, b = 0 }
+
+return {
+    color = {
+        red = red,
+        orange = orange,
+        yellow = yellow,
+        green = green,
+        blue = blue,
+        purple = purple,
+        pink = pink,
+        white = white,
+        black = black
+    }
+}
