fc90e9ca55e94846b4008d127aad415f43378a9e
jingleheimer-schmidt <46385945+jingleheimer-schmidt@users.noreply.github.com>
2023-11-05 00:19:21 -0700

    Create annotations.lua

---
+++ annotations.lua
@@ -0,0 +1,18 @@
+
+---@class task_data
+---@field entity LuaEntity
+---@field entity_id string
+---@field spider LuaEntity
+---@field spider_id string
+---@field task_type string
+---@field player LuaPlayer
+---@field status string
+
+---@alias uuid string|integer
+
+---@class path_request_data
+---@field spider LuaEntity
+---@field spider_id uuid
+---@field entity LuaEntity
+---@field entity_id uuid
+---@field player LuaPlayer
