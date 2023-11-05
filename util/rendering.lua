f30c80bfec4826e371a7817b83d7b5cb7bde58d1
jingleheimer-schmidt <46385945+jingleheimer-schmidt@users.noreply.github.com>
2023-11-05 09:35:32 -0800

    return the render_id

--- util/rendering.lua
+++ util/rendering.lua
@@ -4,10 +4,11 @@
 ---@param to LuaEntity|MapPosition
 ---@param color Color
 ---@param time_to_live integer?
+---@return integer
 local function draw_line(surface, from, to, color, time_to_live)
-    rendering.draw_line({
+    local render_id = rendering.draw_line({
         color = color,
-        width = 6,
+        width = 1.25,
         from = from,
         to = to,
         surface = surface,
@@ -15,6 +16,7 @@ local function draw_line(surface, from, to, color, time_to_live)
         draw_on_ground = true,
         only_in_alt_mode = true,
     })
+    return render_id
 end
 
 return {
